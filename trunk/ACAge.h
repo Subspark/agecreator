225c7281e72e83bff343b2bb154a3af67ecf925b
branan <branan@24beefbe-1c21-11df-b275-e7300661a1c9>
2010-03-18 19:37:23 +0000

    Make object names editable in the object list

--- trunk/ACAge.h
+++ trunk/ACAge.h
@@ -70,17 +70,12 @@ public:
 
   // Model/View functions
   virtual int columnCount(const QModelIndex &parent = QModelIndex()) const;
+  virtual Qt::ItemFlags flags(const QModelIndex &index) const;
   virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+  virtual bool setData(const QModelIndex & index, const QVariant & value, int role = Qt::EditRole);
   virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
   virtual QModelIndex parent(const QModelIndex &index) const;
   virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
-
-  enum
-  {
-    Created,
-    Exists,
-    NoParent,
-  };
 
 public slots:
   void addLayer(QString name = QString());
