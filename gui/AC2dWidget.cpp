4a8baa0f078eba0ac7508b61b5249a0dc94c005d
branan <branan@24beefbe-1c21-11df-b275-e7300661a1c9>
2010-03-20 23:10:57 +0000

    Implement basic preview render

--- gui/AC2dWidget.cpp
+++ gui/AC2dWidget.cpp
@@ -18,6 +18,7 @@
 */
 
 #include "AC2dWidget.h"
+#include "ACObject.h"
 #include "ACAge.h"
 
 AC2dWidget::AC2dWidget(QWidget *parent)
@@ -32,11 +33,59 @@ void AC2dWidget::setAge(ACAge *age)
 void AC2dWidget::initializeGL()
 {
   qglClearColor(palette().color(QPalette::Base));
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+}
+
+void AC2dWidget::resizeGL(int w, int h)
+{
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  //TODO make this match the URU perspective matrix
+  if(w > h) {
+    size_w = 50.0*(double(w)/double(h));
+    size_h = 50.0;
+  } else {
+    size_w = 50.0;
+    size_h = 50.0*(double(h)/double(w));
+  }
+  glOrtho(-size_w, size_w, -size_h, size_h, -10000.0, 10000.0);
+  glMatrixMode(GL_MODELVIEW);
 }
 
 void AC2dWidget::paintGL()
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  int w = size_w;
+  int h = size_h;
+  glBegin(GL_LINES);
+  qglColor(palette().color(QPalette::BrightText));
+  glVertex2f(-size_w, 0.0f);
+  glVertex2f(size_w, 0.0f);
+  glVertex2f(0.0f, -size_h);
+  glVertex2f(0.0f, size_h);
+  qglColor(palette().color(QPalette::Text));
+  for(int i = 5; i < h; i+=5) {
+    glVertex2f(-size_w, float(i));
+    glVertex2f(size_w, float(i));
+    glVertex2f(-size_w, float(-i));
+    glVertex2f(size_w, float(-i));
+  }
+  for(int i = 5; i < w; i+=5) {
+    glVertex2f(float(i), -size_h);
+    glVertex2f(float(i), size_h);
+    glVertex2f(float(-i), -size_h);
+    glVertex2f(float(-i), size_h);
+  }
+  glEnd();
+
+  if(!current_age)
+    return;
+
+  glColor3f(1.0f, 1.0f, 1.0f);
+  for(int i = 0; i < current_age->layerCount(); i++)
+    for(int j = 0; j < current_age->getLayer(i)->objectCount(); j++)
+      current_age->getLayer(i)->getObject(j)->draw();
 }
 
 void AC2dWidget::contextMenuEvent(QContextMenuEvent* event)
