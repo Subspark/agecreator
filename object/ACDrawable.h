50ab1f9d1687f8607694914c343712db9044fbe6
branan <branan@24beefbe-1c21-11df-b275-e7300661a1c9>
2010-03-22 18:26:19 +0000

    Add support for different draw modes

--- object/ACDrawable.h
+++ object/ACDrawable.h
@@ -66,7 +66,7 @@ public:
   void setMeshData(const hsTArray<plGBufferVertex> &verts, const hsTArray<unsigned short> &indices, unsigned char fmt);
   bool loadFromFile(const QString &filename);
   
-  virtual void draw() const;
+  virtual void draw(DrawMode draw) const;
   virtual QIcon icon() const;
   virtual void registerWithPage(ACPage* page);
   virtual void unregisterFromPage(ACPage* page);
